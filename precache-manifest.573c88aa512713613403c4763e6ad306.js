b38510237f10d097b7e85ada4ea2987ae49cece4
DaisukeMukumoto <daisuke.mukumoto@gmail.com>
2020-08-10 16:40:23 +0000

    Add dist

---
+++ precache-manifest.573c88aa512713613403c4763e6ad306.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceca75dcb6f8bf4f155f",
+    "url": "css/app.8caa578c.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "80c830c8db462a9de38d14a5743b36c4",
+    "url": "index.html"
+  },
+  {
+    "revision": "ceca75dcb6f8bf4f155f",
+    "url": "js/app.9f31fb2c.js"
+  },
+  {
+    "revision": "9e83726d6b1a868d032f",
+    "url": "js/chunk-vendors.bdc9172a.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
